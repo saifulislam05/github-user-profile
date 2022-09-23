01b95a3c04a8c55d663b0138961cae3a00f0738b
saifulislam05 <saiful.bijulia@gmail.com>
2022-09-24 00:33:49 +0530

    detch data

--- src/components/fullbody/index.js
+++ src/components/fullbody/index.js
@@ -1,7 +1,37 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-import React from "react";
 const FullBody = () => {
 
+    const [inputVal, setInputVal] = useState();
+    const [user, setUser] = useState();
+
+
+    console.log(inputVal);
+
+
+// set input value
+    const handleValue = e => {
+        setInputVal(e.target.value)
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(
+            `https://api.github.com/users/${inputVal}`
+        );
+        const data = await response.json();
+        setUser(data);
+    };
+
+    useEffect(() => {
+        
+        handleSubmit();
+    }, []);
+
+
+    console.log(user.name);
+    
 
     return (
         <div className="flex justify-center items-center w-full h-screen text-slate-200">
@@ -10,29 +40,29 @@ const FullBody = () => {
                 {/* input start  */}
                 <div className="w-full flex justify-center">
                     <form className=" flex w-4/6 justify-between">
-                        <input id="input" type="text" placeholder="Enter your github username" className="flex-6 w-8/12 bg-slate-700 p-2 px-4 outline-none rounded-full" />
-                        <button id="btn" className="w-3/12 bg-slate-100 text-slate-800 font-bold p-2 rounded-full " >Submit</button>
+                        <input id="input" type="text" placeholder="Enter your github username" className="flex-6 w-8/12 bg-slate-700 p-2 px-4 outline-none rounded-full" onChange={e=>handleValue(e)} />
+                        <button id="btn" className="w-3/12 bg-slate-100 text-slate-800 font-bold p-2 rounded-full" onClick={handleSubmit} >Submit</button>
                     </form>
                 </div>
                 {/* Input end */}
                 <div className="mt-6" >
                     <div className="w-full flex justify-between">
-                        <img id="image" src='https://avatars.githubusercontent.com/u/73392705?v=4' alt="user" className="w-3/12 rounded-full  " />
+                        <img id="image" src={user.avatar_url} alt="user" className="w-3/12 rounded-full  " />
                         <div className="w-8/12 ">
                             <div className=" flex justify-between mb-4">
                                 <div>
-                                    <p id="name" className="text-3xl font-semibold">Saiful islam</p>
-                                    <p id="userName" className="text-sm my-1">saifulislam05</p>
+                                    <p id="name" className="text-3xl font-semibold">{user.name}</p>
+                                    <p id="userName" className="text-sm my-1">{user.login}</p>
                                 </div>
                                 <p id="joinDate">joined 21050545</p>
                             </div>
                             <div>
-                                <p id="bio" className="text-md my-2">sorry we are not getting anything of you bio</p>
+                                <p id="bio" className="text-md my-2">{user.bio}</p>
                             </div>
                             <div className="flex justify-between w-full text-center">
                                 <div className="">
                                     <h1 >Total Repositories</h1>
-                                    <b id="repos">19</b>
+                                    <b id="repos">{user.repos_url?.id}</b>
                                 </div>
                                 <div className="">
                                     <h1>Followers</h1>
