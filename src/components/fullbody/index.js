2243857f9d38334e5e547fcff339342f79c4099c
saifulislam05 <saiful.bijulia@gmail.com>
2022-09-24 21:05:58 +0530

    modified some design and contidion

--- src/components/fullbody/index.js
+++ src/components/fullbody/index.js
@@ -3,66 +3,88 @@ import { useState } from 'react';
 
 const FullBody = () => {
 
-    const [inputVal, setInputVal] = useState();
-    const [user, setUser] = useState();
+    const [inputVal, setInputVal] = useState('');
+    const [userData, setUserData] = useState(Object);
 
 
-    console.log(inputVal);
-
-
-// set input value
+    // set input value
     const handleValue = e => {
         setInputVal(e.target.value)
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const response = await fetch(
-            `https://api.github.com/users/${inputVal}`
-        );
-        const data = await response.json();
-        setUser(data);
-    };
-
     useEffect(() => {
-        
         handleSubmit();
-    }, []);
+    })
+    var api = `https://api.github.com/users/${inputVal}`
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    const getUserData = async () => {
+        const response = await fetch(api);
+        const jsonData = await response.json();
+        if (jsonData && jsonData.message !== "Not Found") {
+            setUserData(jsonData);
+            console.log(jsonData);
+        } else if (inputVal !== "") {
+            console.log("Username dose not exist");
+        }
+        else {
+            setUserData({})
+        }
+        }
+        getUserData()
+    }
 
 
-    console.log(user.name);
+    console.log(userData.name);
     
 
     return (
         <div className="flex justify-center items-center w-full h-screen text-slate-200">
-            <div className='bg-slate-800 w-6/12 h-[500px] border-black p-6 px-8 rounded-xl'>
+            <div className='bg-slate-800 md:w-6/12 h-[500px] border-black p-6 px-8 rounded-xl'>
                 <h1 className="text-center text-2xl font-bold mb-6 ">Github User Profile</h1>
                 {/* input start  */}
                 <div className="w-full flex justify-center">
                     <form className=" flex w-4/6 justify-between">
-                        <input id="input" type="text" placeholder="Enter your github username" className="flex-6 w-8/12 bg-slate-700 p-2 px-4 outline-none rounded-full" onChange={e=>handleValue(e)} />
+                        <input
+                            id="input"
+                            type="text"
+                            placeholder="Enter your github username"
+                            className="flex-6 w-8/12 bg-slate-700 p-2 px-4 outline-none rounded-full"
+                            onChange={e => handleValue(e)}
+                            onKeyUp={e => handleValue(e)}
+                            onPaste={e => handleValue(e)}
+                        />
                         <button id="btn" className="w-3/12 bg-slate-100 text-slate-800 font-bold p-2 rounded-full" onClick={handleSubmit} >Submit</button>
                     </form>
                 </div>
-                {/* Input end */}
+
+                {/*-------------------------- Data output after fetching ------------------------------- */}
+                
                 <div className="mt-6" >
                     <div className="w-full flex justify-between">
-                        <img id="image" src={user.avatar_url} alt="user" className="w-3/12 rounded-full  " />
+                        {/* user Image  */}
+                        {userData.avatar_url && <img id="image" src={userData.avatar_url} alt="user" className="w-3/12 rounded-full  " />}
                         <div className="w-8/12 ">
                             <div className=" flex justify-between mb-4">
                                 <div>
-                                    <p id="name" className="text-3xl font-semibold">{user.name}</p>
-                                    <p id="userName" className="text-sm my-1">{user.login}</p>
+                                    {/* Name of the user */}
+                                    {userData.name && <p id="name" className="text-3xl font-semibold">{userData.name ? userData.name : `Saiful islam`}</p>}
+                                    {/* Username of the user  */}
+                                    {userData.login && <p id="userName" className="text-sm my-1">{userData.login}</p>}
                                 </div>
-                                <p id="joinDate">joined 21050545</p>
+                                {/* Joined date at github date */}
+                                {userData.created_at &&
+                                    <p id="joinDate"> joined <br />{new Date(userData.created_at).toLocaleDateString()}</p>}
                             </div>
                             <div>
-                                <p id="bio" className="text-md my-2">{user.bio}</p>
+                                    {/* Bio/description of the user  */}
+                                {userData.bio && <p id="bio" className="text-md my-2">{userData.bio}</p>}
                             </div>
                             <div className="flex justify-between w-full text-center">
                                 <div className="">
-                                    <h1 >Total Repositories</h1>
-                                    <b id="repos">{user.repos_url?.id}</b>
+                                    <h1 >Total Repositories</h1>0
+                                    <b id="repos">{userData.repos_url?.id}</b>
                                 </div>
                                 <div className="">
                                     <h1>Followers</h1>
